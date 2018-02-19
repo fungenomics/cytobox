8abdc1f83688d7f29fd737f7780e39cce7a17bd0
Alexis Blanchet-Cohen <alexis.blanchet-cohen@mail.mcgill.ca>
2018-02-19 17:01:10 -0500

    Added labelTSNEClusters function.

---
+++ R/labelTSNEClusters.R
@@ -0,0 +1,27 @@
+# Function to label tSNE clusters.
+
+#' labelTSNEClusters
+#'
+#' @param object Seurat object
+#' @param labelsFile Path to labels file
+#'
+#' @return tSNE plot with labelled clusters
+#'
+#' @export
+#' @author Alexis Blanchet-Cohen
+labelTSNEClusters <- function(seurat, labelsFile) {
+
+    labels <- data.table::fread(labelsFile)
+
+    old.cluster.ids <- levels(unique(GetClusters(seurat)$cluster))
+    new.cluster.ids <- old.cluster.ids
+    new.cluster.ids[labels$cluster+1] <- labels$label
+
+    if (colors %in% labels$color) {
+     # Selected colors.
+        p <- TSNEPlot(object = data, do.label = FALSE, pt.size = 0.5, colors.use=labels$color, do.return=TRUE)
+    } else {
+        p <- TSNEPlot(object = data, do.label = FALSE, pt.size = 0.5, do.return=TRUE)
+    }
+    return(p)
+}
