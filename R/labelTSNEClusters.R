f853e3b87d6937554047791b10ac3bf46b8ba576
blancha <alexis.blanchet-cohen@ircm.qc.ca>
2018-02-21 07:42:17 -0500

    Fixed bugs

--- R/labelTSNEClusters.R
+++ R/labelTSNEClusters.R
@@ -2,22 +2,21 @@
 
 #' labelTSNEClusters
 #'
-#' @param seurat Seurat object
-#' @param labelsFile Path to labels file
+#' @param seurat Seurat object.
+#' @param labels A data frame with 3 columns: "cluster", "label" and "color". The "color" column is optional.
+#' @param colors Cluster colors (Optional).
 #'
-#' @return tSNE plot with labelled clusters
+#' @return A ggplot2 object. A tSNE plot with labelled clusters
 #'
 #' @export
 #' @author Alexis Blanchet-Cohen
-labelTSNEClusters <- function(seurat, labelsFile) {
-
-    labels <- data.table::fread(labelsFile)
+labelTSNEClusters <- function(seurat, labels) {
 
     old.cluster.ids <- levels(unique(GetClusters(seurat)$cluster))
     new.cluster.ids <- old.cluster.ids
     new.cluster.ids[labels$cluster+1] <- labels$label
 
-    if (colors %in% labels$color) {
+    if ("color" %in% labels$color) {
      # Selected colors.
         p <- TSNEPlot(object = seurat, do.label = FALSE, pt.size = 0.5, colors.use=labels$color, do.return=TRUE)
     } else {
