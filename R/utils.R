8515f2b047e6ad5d9d9e6d6a4be52ea69b29f721
sjessa <selinjessa@gmail.com>
2018-03-05 12:10:04 -0500

    Improve plotting with percentiles, add shortcut function feature()

--- R/utils.R
+++ R/utils.R
@@ -1,39 +1,7 @@
 # Helper and utility functions
 
-#' ggColours
-#'
-#' Get evenly spaced colours from around the colour wheel, which are the default
-#' colours assigned to clusters by Seurat. The output of this function can be
-#' passed to the \code{scale_colour_manual()} and \code{scale_fill_manual()} functions
-#' from ggplot2, as the \code{values} argument. (\code{\link{ggColors}} points
-#' to this function.)
-#'
-#' @param n Number of colours to return
-#'
-#' @return Named character vector, where names are the names of clusters, from
-#' 0 to n-1, and values are the hex codes for the colours.
-#' @export
-#'
-#' @examples
-#'
-#' n_clust <- 5
-#' ggColours(n_clust)
-#'
-#' @references https://stackoverflow.com/a/8197703
-#' @aliases ggColors
-#' @importFrom grDevices hcl
-ggColours <- function(n) {
 
-    hues <- seq(15, 375, length = n + 1)
-    colours <- hcl(h = hues, l = 65, c = 100)[1:n]
-    names(colours) <- seq(0, n - 1) # Since the first cluster in Seurat is 0
 
-    return(colours)
-
-}
-
-#' @export
-ggColors <- ggColours
 
 
 #' addEmbedding
@@ -62,6 +30,8 @@ addEmbedding <- function(seurat, df, reduction = "tsne") {
     # Get the axes for the reduced space
     # See here: http://dplyr.tidyverse.org/articles/programming.html#setting-variable-names
     vars <- colnames(seurat@dr[[reduction]]@cell.embeddings)[c(1, 2)]
+
+    df$Cell <- as.character(df$Cell)
 
     embedding <- data.frame(Cell = seurat@cell.names, stringsAsFactors = FALSE) %>%
         mutate(!!vars[1] := seurat@dr[[reduction]]@cell.embeddings[, 1],
@@ -169,33 +139,5 @@ findGenes <- function(seurat, genes) {
 
 
 
-#' theme_min
-#'
-#' A clean theme for ggplot2
-#' @references https://github.com/sjessa/ggmin
-#'
-#' @importFrom ggplot2 theme_light theme
-#' @author Selin Jessa
-#' @export
-theme_min <- function(base_size = 11, base_family = "") {
 
-    theme_light(base_size = 11, base_family = "") +
-        theme(
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.background = element_blank(),
-            panel.border = element_rect(fill = NA, colour = "grey90", size = 1),
-            strip.background = element_rect(fill = NA, colour = NA),
-            strip.text.x = element_text(colour = "black", size = rel(1.2)),
-            strip.text.y = element_text(colour = "black", size = rel(1.2)),
-            title = element_text(size = rel(0.9)),
-            axis.text = element_text(colour = "black", size = rel(0.8)),
-            axis.title = element_text(colour = "black", size = rel(0.9)),
-            legend.title = element_text(colour = "black", size = rel(0.9)),
-            legend.key.size = unit(0.9, "lines"),
-            legend.text = element_text(size = rel(0.7), colour = "black"),
-            legend.key = element_rect(colour = NA, fill = NA),
-            legend.background = element_rect(colour = NA, fill = NA)
-        )
-}
 
